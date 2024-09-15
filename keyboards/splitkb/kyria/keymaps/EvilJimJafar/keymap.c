847584eea51428494f11ef4859019917c88df788
Jim Sangwine <jim@sangwine.net>
2024-09-15 22:56:23 +0100

    ctrl+tab

--- keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
+++ keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
      KC_ESC  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U   ,  KC_I ,   KC_O ,  KC_P ,  KC_GRV,
-     KC_TAB  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J   ,  KC_K ,   KC_L ,KC_SCLN,KC_QUOTE,
+     CTL_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J   ,  KC_K ,   KC_L ,KC_SCLN,KC_QUOTE,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,  WIN  ,     FKEYS  , KC_RBRC, KC_N,   KC_M   ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 KC_LALT, KC_LGUI,BSP_SHFT,  KC_ENT,  NAV  ,     SYM    , KC_ENT ,SPC_SHFT,KC_RGUI,KC_RALT
     ),
