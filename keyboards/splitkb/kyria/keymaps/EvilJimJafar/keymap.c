4a77bb3616910f892734dc56841de0e926aa5799
Jim Sangwine <jim@sangwine.net>
2024-09-15 18:37:20 +0100

    Update kyria layout

--- keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
+++ keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
@@ -42,6 +42,7 @@ enum layers {
 #define CTL_TAB MT(MOD_RCTL, KC_TAB)
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
+#define GBP      MT(MOD_LALT, KC_3)
 
 // Raycast window shortcuts
 #define RW_SCR LCTL(LGUI(KC_U)) // next screen
@@ -148,9 +149,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |    `   |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |   =    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |    ~   |  !   |  @   |  #   |  $   |  %   |                              |   ^  |  &   |  *   |  (   |  )   |   +    |
+ * |    ~   |  {   |  (   |  [   |  <   |  \   |                              |   /  |  >   |  ]   |  )   |  }   |   +    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |    |   |   \  |  :   |  ;   |  -   |  [   |  {   |      |  |      |   }  |   ]  |  _   |  ,   |  .   |  /   |   ?    |
+ * |    |   |   £  |  $   |  @   |  #   |  %   |  !   |      |  |      |   *  |   &  |  _   |  -   |  .   |  /   |   ?    |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
@@ -158,8 +159,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_SYM] = LAYOUT(
       KC_GRV ,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_EQL ,
-     KC_TILD , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-     KC_PIPE , KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
+     KC_TILD , KC_LCBR, KC_LPRN, KC_LBRC, KC_LABK, KC_BSLS,                                     KC_SLSH, KC_RABK, KC_RBRC, KC_RPRN, KC_RCBR, KC_PLUS,
+     KC_PIPE ,     GBP,  KC_DLR,   KC_AT, KC_HASH, KC_PERC, KC_EXLM, _______, _______, KC_ASTR, KC_AMPR, KC_UNDS, KC_MINS,  KC_DOT, KC_SLSH, KC_QUES,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
