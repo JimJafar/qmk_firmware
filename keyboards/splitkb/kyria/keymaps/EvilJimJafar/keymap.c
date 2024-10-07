b21e66502cda314325896b85121ccb003f8a6e7c
Jim Sangwine <jim@sangwine.net>
2024-10-07 15:53:26 +0100

    Update kyria layout

--- keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
+++ keyboards/splitkb/kyria/keymaps/EvilJimJafar/keymap.c
@@ -13,6 +13,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*
+ * TODO:
+ * - left-CMD + Esc -> CMD + `
+ * - left-CMD + Backspace -> Enter
+*/
+
 #include QMK_KEYBOARD_H
 
 enum layers {
