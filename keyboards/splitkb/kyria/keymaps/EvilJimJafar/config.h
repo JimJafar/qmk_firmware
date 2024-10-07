f70468228dde508718c164d506470c453d24a684
Jim Sangwine <jim@sangwine.net>
2024-10-07 23:16:40 +0100

    Update kyria layout

--- keyboards/splitkb/kyria/keymaps/EvilJimJafar/config.h
+++ keyboards/splitkb/kyria/keymaps/EvilJimJafar/config.h
@@ -28,9 +28,9 @@
 #undef MOUSEKEY_WHEEL_DELAY
 #define MOUSEKEY_WHEEL_DELAY    0
 #undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED      6
+#define MOUSEKEY_MAX_SPEED      4
 #undef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX    64
+#define MOUSEKEY_TIME_TO_MAX    96
 
 
 // Home row mods
